67a160fc4d9ce299e0601e4681073b1b636d09ad
saz53 <60635932+saz53@users.noreply.github.com>
2021-04-07 19:49:09 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from datetime import datetime
+
+app = FastAPI()
+
+@app.get('/currenttime')
+def get_current_time():
+    now = datetime.now()
+    current_time = now.strftime("%A %B %d, %H:%M:%S")
+    return {'time': current_time}
